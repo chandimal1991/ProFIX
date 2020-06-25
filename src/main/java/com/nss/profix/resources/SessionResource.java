7051161353b685451c90e9cd98ff6c3b70ad353d
Rasika Ranepura <rcranepura@gmail.com>
2020-06-25 09:46:18 +0530

    change web.xml

--- src/main/java/com/nss/profix/resources/SessionResource.java
+++ src/main/java/com/nss/profix/resources/SessionResource.java
@@ -1,5 +1,6 @@
 package com.nss.profix.resources;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.GET;
@@ -8,20 +9,24 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.nss.profix.fixservice.FIXService;
+import com.nss.profix.model.Session;
 import com.nss.profix.service.FIXServiceImpl;
+import com.nss.profix.service.SessionService;
 
 @Path("/session")
 public class SessionResource {
 	
 	FIXService fixServiceImpl = new FIXServiceImpl();
-	
-	/*@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public ArrayList<Session> getSessionList() {
-		return sessionService.getSessionList();
-	}*/
+	SessionService sessionService = new SessionService();
 	
 	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<Session> getSessions() {
+		return sessionService.getSessionList();
+	}
+	
+	@GET
+	@Path("/list")
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<String> getSessionList() {
 		return fixServiceImpl.getSessionList();
